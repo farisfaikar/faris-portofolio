aa8d9b1c8e937a380cd2be0bc8b0cbd2110bf92f
R0merol <farisfrontiers@gmail.com>
2023-03-05 18:28:12 +0700

    added go to blog and home links

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    date = models.DateField()
+    read_duration = models.TextField()
+    content = models.TextField()
+    label = models.TextField()
